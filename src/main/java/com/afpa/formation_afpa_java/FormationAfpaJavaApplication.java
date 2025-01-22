056190940beed98021b42a23564bc319949bd6a0
Aloeverras <edouard.bouche@gmail.com>
2025-01-22 08:31:16 +0100

    init project

---
+++ src/main/java/com/afpa/formation_afpa_java/FormationAfpaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.afpa.formation_afpa_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationAfpaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationAfpaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afpa/formation_afpa_java/FormationAfpaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afpa.formation_afpa_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationAfpaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
